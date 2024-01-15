2a70aaddc6363e87c83b13bc12bd0510d0348479
Landon Metcalf <lndncole@gmail.com>
2024-01-14 17:23:16 -0800

    Create db directory

---
+++ db/db.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+
+module.exports = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
--- server/server.js
+++ server/server.js
@@ -1,34 +1,25 @@
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
+const pool = require('../db/db.js');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(bodyParser.json());
 
 
 const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
- res.send('Hello World!');
-});
 
 app.listen(port, () => {
  console.log(`Server running on port ${port}`);
 });
 
-
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
   
