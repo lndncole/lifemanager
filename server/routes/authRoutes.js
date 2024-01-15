297260bff39bac020ec058be9d00705ebd88124b
Landon Metcalf <lndncole@gmail.com>
2024-01-15 13:41:00 -0800

    Add ana route for fun

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -16,9 +16,12 @@ router.get('/home', (req, res) => {
   res.send('Welcome to the Home Page');
 });
 
-// Home Route - After Successful Sign In
 router.get('/chick', (req, res) => {
   res.send('Chick Fil A breakfast tomorrow?');
+});
+
+router.get('/ana', (req, res) => {
+  res.send("I love Ana more than any girl in the entire universe. She is so sweet and beautiful and I'm lucky to have her in my life");
 });
 
 module.exports = router;
