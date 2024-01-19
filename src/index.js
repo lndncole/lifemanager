5773b8f987051a410e69cfc9a72823cf29c5abfe
Landon Metcalf <lndncole@gmail.com>
2024-01-18 20:03:23 -0800

    Remove unused Calendar component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Calendar from './Calendar'; // Import the Calendar component
 import { createRoot } from 'react-dom/client';
 import HomeComonent from './HomeComponent';
 const container = document.getElementById('root');
