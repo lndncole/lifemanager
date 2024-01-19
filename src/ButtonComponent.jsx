4848a0775f6ac6af94557d16980085e3914740e8
Landon Metcalf <lndncole@gmail.com>
2024-01-19 11:53:06 -0800

    Set prod domain for buttonComponent

--- src/ButtonComponent.jsx
+++ src/ButtonComponent.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 // Set the domain
-// const domain = "https://lifemanager-c8d019eb99cb.herokuapp.com";
-const domain = "http://localhost:8080";
+const domain = "https://lifemanager-c8d019eb99cb.herokuapp.com";
+// const domain = "http://localhost:8080";
 const ButtonComponent = () => {
     const handleClick = () => {
         window.location.href = `${domain}/calendar`;
