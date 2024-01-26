321ceffc24581802b94ada520e90cd751dc77a7b
Landon Metcalf <lndncole@gmail.com>
2024-01-25 23:51:03 -0800

    More styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,8 @@ const About = () => {
   return (
     <div class="f-col full-screen">
       <h1>about lifeMNGR</h1>
+      <p>Find our GPT in the GPT+ marketplace, here: <a href="https://chat.openai.com/g/g-Hnp8qMlcq-lifemngr">lifeMNGR GPT 4</a>
+      </p>
     </div>
   );
 };
