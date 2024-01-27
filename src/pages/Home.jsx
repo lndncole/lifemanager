53d21f952318a4d6a35f0d790bd57a8cbf3c8d69
Landon Metcalf <lndncole@gmail.com>
2024-01-27 01:45:22 -0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import '../styles/home.css';
-import { castToError } from 'openai/core';
 
 const Home = () => {
   const [eventName, setEventName] = useState('');
