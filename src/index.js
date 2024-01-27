52ee27d36cda8f77a743627a75fc5a9c331adb7b
Landon Metcalf <lndncole@gmail.com>
2024-01-27 11:56:30 -0800

    Install GA4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 //src/index.js
+//Dependencies
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import ReactGA from 'react-ga4';
+
+//Components
 import AuthenticatedRoute from './components/AuthenticatedRoute';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
+//Pages
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import About from './pages/About';
+
+//Styles
 import './styles/global.css';
+
+//Init middleware
+ReactGA.initialize('G-S551MB5RNK');
 
 const App = () => {
   return (
