8b1191f96adb5fc1cfec22163f8a48b76ba1c2c9
Landon Metcalf <lndncole@gmail.com>
2024-02-17 21:33:12 -0800

    Using all CRUD operations here

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,7 @@
 require('dotenv').config({ path: '../../.env' });
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-console.log(process.env.MONGO_PW);
-
-const uri = `mongodb+srv://root:${process.env.MONGO_PW}@lifemngr-website.bbhe50q.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGO_UN}:${process.env.MONGO_PW}@lifemngr-website.bbhe50q.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,12 +16,60 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //Issuing commands to the admin db gives you global information
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    //List DB's / collections (db's are the same as collections)
+    // const databases = await client.db("admin").command( { listDatabases: 1 } );
+
+
+    //Create db - If the database doesn’t exist yet, it will be created.
+    const db = client.db('users');
+
+    //Create or select collection
+    const collection = db.collection('unser_info');
+
+
+    //Find entry in db
+    const name = 'John Smith';
+    // const foundEntry = await collection.find({ name }).toArray();
+    // console.log(foundEntry);
+
+
+    //Add entry to db 
+    const testData = {
+      name: 'John Smith',
+      email: 'john.smith@gmail.com',
+      profilePicUrl: 'www.google.com/profilepicurl/johnsmith'
+    };
+    // const insertOne = await collection.insertOne(testData);
+    // console.log(insertOne);
+
+
+    //Delete entry from db
+    const deletedEntry = await collection.deleteMany({ name });
+    console.log(deletedEntry);
+
+    //Update entry in db
+    // const updatedEntry = await collection.updateMany(
+    //   { name },
+    //   { $set: { birthdate: 'jan 4, 1987'} }
+    // );
+    // console.log(updatedEntry);
+
+
+
+
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
-run().catch(console.dir);
+
+run();
+module.exports = { run }
