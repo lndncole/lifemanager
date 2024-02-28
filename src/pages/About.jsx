44355308c2958f4462ff63580c7dd9cc7dd268da
Landon Metcalf <lndncole@gmail.com>
2024-02-28 14:54:12 -0800

    Update about page positioning

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,9 @@ const About = () => {
   return (
     <div class="f-col full-screen">
       <h1>about lifeMNGR</h1>
-      <p>Find our GPT in the GPT+ marketplace, <br>
+      <div>Find our GPT in the GPT+ marketplace, <br>
         </br>here: <a class="link" href="https://chat.openai.com/g/g-Hnp8qMlcq-lifemngr">lifeMNGR GPT 4</a>
-      </p>
+      </div>
     </div>
   );
 };
